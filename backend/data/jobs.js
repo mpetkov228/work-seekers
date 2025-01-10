314415a8b861bc928e641f1f89fd49d61f5b53db
MihailPetkov96 <mpetkov228@gmail.com>
2025-01-10 23:16:12 +0200

    Add some hardcoded data for jobs

---
+++ backend/data/jobs.js
@@ -0,0 +1,26 @@
+const jobs = [
+  {
+    id: 1,
+    title: 'Junior Developer',
+    location: 'Prague',
+    salary: '€60000',
+    company: 'Devs Inc',
+    description: 'Busy busy job'
+  },
+  {
+    id: 2,
+    title: 'Bricklayer',
+    location: 'Munich',
+    salary: '€75000',
+    company: 'Bricks and Mortar',
+    description: 'Lay bricks'
+  },
+  {
+    id: 3,
+    title: 'Landscaper',
+    location: 'Paris',
+    salary: '€64000',
+    company: 'Gardens LLC',
+    description: 'Make gardens pretty'
+  },
+];
