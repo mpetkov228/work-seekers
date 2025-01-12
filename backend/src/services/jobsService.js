c77b83767642d9118c6105b865f33bc64824a198
MihailPetkov96 <mpetkov228@gmail.com>
2025-01-12 19:48:41 +0200

    Add create to job service

--- backend/src/services/jobsService.js
+++ backend/src/services/jobsService.js
@@ -1,9 +1,20 @@
 const data = require('../../data/jobs');
+const uuid = require('uuid');
 
 const getAll = () => {
   return data;
 };
 
+const create = (job) => {
+  const newJob = {
+    id: uuid(),
+    ...job
+  };
+  data.push(newJob);
+
+  return newJob;
+};
+
 module.exports = {
-  getAll
+  getAll, create
 };
